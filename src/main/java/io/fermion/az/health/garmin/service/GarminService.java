f44e35d2e7d4e95cd3d854029606a445f33dc7cc
Alson1109 <168754277+Alson1109@users.noreply.github.com>
2025-10-10 15:56:19 +0800

    Update GarminService.java

--- src/main/java/io/fermion/az/health/garmin/service/GarminService.java
+++ src/main/java/io/fermion/az/health/garmin/service/GarminService.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
