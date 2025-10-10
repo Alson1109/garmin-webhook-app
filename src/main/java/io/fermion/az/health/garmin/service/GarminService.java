a62fc88489ea611bdbba06b091d4fcae3e87e660
Alson1109 <168754277+Alson1109@users.noreply.github.com>
2025-10-10 15:00:45 +0800

    Update GarminService.java

--- src/main/java/io/fermion/az/health/garmin/service/GarminService.java
+++ src/main/java/io/fermion/az/health/garmin/service/GarminService.java
@@ -22,6 +22,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map; 
 import java.util.Base64;
 
 @Service
